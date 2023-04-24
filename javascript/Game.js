ff5d0f6625e783aa96e83bb22814379b2f796ed0
alvaritu <alvarohegon@gmail.com>
2023-04-24 14:10:29 +0200

    game loop

--- javascript/Game.js
+++ javascript/Game.js
@@ -1,10 +1,33 @@
+// propiedades de Game.js => todos los elementos q existen en el juego,
 class Game {
+    constructor() {
+        // el constructor es TODOS los elementos y valores iniciales del juego.
+        //el fono
+        this.background = new Image();
+        this.background.src = "images/background.jpg";
 
+        this.isGameOn = true;
+        //! MVP
+        //los aviones 
+        //la torreta 
+        //la bala
+        
+    }
 
-
+    //! MVP
+    // metodos d Game => todas las acciones q se realizan en el juego.
+    // aparecen los aviones de mnera aleatoria
+    // que el cañón dispare
+    // colisiones de la bala con los aviones
 
     gameLoop = () => {
         console.log("ejecutando recursión del juego");
+
+        // 1. limpieza del canvas
+        // 2. acciones y movimientos d los elementos 
+        // 3. dibujad d los elementos
+        // 4. recursion
+        requestAnimationFrame(this.gameLoop)
     }
 
 }
--- javascript/main.js
+++ javascript/main.js
@@ -1,1 +1,29 @@
-const ctx = canvas.getContext("2d")
+// * GLOBAL VARIABLES
+
+const splashScreenDOM = document.querySelector("#splash-screen");
+const gameoverScreenDOM = document.querySelector("#gameover-screen");
+const startBtnDOM = document.querySelector("#start-btn");
+const canvasDOM = document.querySelector("#my-canvas");
+
+const ctx = canvasDOM.getContext("2d");
+
+// *STATE MANAGEMENT FUNCTIONS
+
+const startGame = () => {
+    console.log("intentando iniciar el juego");
+
+    // cambiar las pantallas d juego
+    splashScreenDOM.style.display = "none";
+    canvasDOM.style.display = "block";
+    // crear los elementos del juego
+    //vamos a crear una clase
+    let gameObj = new Game()
+    console.log(gameObj);
+    // iniciar el bucle del juego (recursion)
+    gameObj.gameLoop()
+
+
+};
+
+// *ADD EVENT LISTENER
+startBtnDOM.addEventListener("click", startGame);
